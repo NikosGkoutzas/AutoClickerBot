7bce7c29275bb2b287093ef3af485b20942026f2
Nikos Gkoutzas <nickgkoutzas@gmail.com>
2026-01-18 20:39:59 +0200

    update 2 Jan 18, 2026

--- app/email/process_emails.py
+++ app/email/process_emails.py
@@ -174,13 +174,13 @@ class ProcessEmails(ProcessEmailsInterface):
                         shutil.move(os.path.join(source_dir , item) , destination_dir)
 
             #BASE_DIR = os.path.join(os.getcwd() , 'AutoClickerBot')
-            '''BASE_DIR = f'{os.path.expanduser("~")}/Music/AutoClickerBot/'
+            BASE_DIR = f'{os.path.expanduser("~")}/Music/AutoClickerBot/'
             print(BASE_DIR)
             os.chdir(BASE_DIR)
             self.write_files.write_app_version(semantic_input)
             self.write_files.write_new_version_update_flag(1)
             print('SUCCESS!')
-            os.execv(sys.executable , [sys.executable, "-m", "app.main"])'''
+            os.execv(sys.executable , [sys.executable, "-m", "app.main"])
         
         except Exception as e:
             print(f'An error occured while trying to install the latest version from github: {str(e)}')
